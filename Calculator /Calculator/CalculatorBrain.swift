acd5f20299bbf3740f6ab07a86d045554ed094e5
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2025-10-26 21:53:04 +0900

    1강 iOS  개요 및 계산기 만들기

--- Calculator /Calculator/CalculatorBrain.swift
+++ Calculator /Calculator/CalculatorBrain.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class CalculatorBrain {
+    
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    func performOperation(symbol: String) {
+        switch symbol {
+        case "𝞹" : accumulator = Double.pi
+        case "√" : accumulator = sqrt(accumulator)
+        default : break
+        }
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
---
+++ Calculator /Calculator/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by YangJeongMu on 10/25/25.
+//
+
+import UIKit // 모듈
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        guard let digit = sender.configuration?.title else { return } // 버튼 누르면 숫자 가 나옴 해당 현재 타이틀을 이용해서
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text! // 기존 숫자 값
+            display.text = textCurrentlyInDisplay + digit // 기존 숫자 값 + 내가 누른 숫자
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!) ?? 0
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        guard let mathematicalSymbol = sender.configuration?.title else { return }
+        brain.performOperation(symbol: mathematicalSymbol)
+        
+        displayValue = brain.result
+       }
+        
+    }
